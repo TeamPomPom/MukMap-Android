c8920b118e77c946cf8518ab910be1a18120b390
handnew <handnew04@gmail.com>
2020-12-21 01:34:59 +0900

    Add theme base xml files

---
+++ app/src/main/java/team/pompom/mukmap/MainActivity.kt
@@ -0,0 +1,11 @@
+package team.pompom.mukmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
