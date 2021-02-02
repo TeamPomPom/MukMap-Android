4cbd1fc7831aba1097e874fde36a4e9ae8b9df1c
yujung <handnew04@gmail.com>
2021-02-02 09:12:14 +0900

    Add global actions and Fix labels #9

--- app/src/main/java/team/pompom/mukmap/ui/MainActivity.kt
+++ app/src/main/java/team/pompom/mukmap/ui/MainActivity.kt
@@ -1,21 +1,32 @@
 package team.pompom.mukmap.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import team.pompom.mukmap.R
 import team.pompom.mukmap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var view : View
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
+        view = binding.root
         setContentView(view)
+
+        navController = view.findNavController()
 
 //        setSupportActionBar()
 //        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 //        supportActionBar?.setDisplayShowHomeEnabled(true)
-
-
     }
 }
