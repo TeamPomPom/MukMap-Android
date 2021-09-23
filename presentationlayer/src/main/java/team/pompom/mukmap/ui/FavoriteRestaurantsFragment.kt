809ad88bba9e8f7f1df4fcac3e7a69702c414533
kangraemin <remin1994@gmail.com>
2021-09-23 21:08:27 +0900

    [Refactor] Use baseFragment for default fragment settings
    - Fix layout resource xml to data binding layout

--- presentationlayer/src/main/java/team/pompom/mukmap/ui/FavoriteRestaurantsFragment.kt
+++ presentationlayer/src/main/java/team/pompom/mukmap/ui/FavoriteRestaurantsFragment.kt
@@ -1,28 +1,9 @@
 package team.pompom.mukmap.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import team.pompom.mukmap.R
+import team.pompom.mukmap.base.BaseFragment
 import team.pompom.mukmap.databinding.FragmentFavoritesBinding
 
-class FavoriteRestaurantsFragment : Fragment() {
-    private var _binding: FragmentFavoritesBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
+class FavoriteRestaurantsFragment : BaseFragment<FragmentFavoritesBinding>(
+    layoutRes = R.layout.fragment_favorites
+)
--- presentationlayer/src/main/java/team/pompom/mukmap/ui/RestaurantFragment.kt
+++ presentationlayer/src/main/java/team/pompom/mukmap/ui/RestaurantFragment.kt
@@ -1,28 +1,9 @@
 package team.pompom.mukmap.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import team.pompom.mukmap.R
+import team.pompom.mukmap.base.BaseFragment
 import team.pompom.mukmap.databinding.FragmentDetailRestaurantBinding
 
-class RestaurantFragment : Fragment() {
-    private var _binding: FragmentDetailRestaurantBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentDetailRestaurantBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
+class RestaurantFragment : BaseFragment<FragmentDetailRestaurantBinding>(
+    layoutRes = R.layout.fragment_detail_restaurant
+)
